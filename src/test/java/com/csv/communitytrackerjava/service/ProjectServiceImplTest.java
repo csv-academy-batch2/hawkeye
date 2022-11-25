a98685b06bcfb3ae17871388ed0dc00a4ca669c7
jhonlyoyd <jhonlyoyd.alejo@softvision.com>
2022-11-25 18:05:25 +0800

    Refining the ProjecetServiceImpl

---
+++ src/test/java/com/csv/communitytrackerjava/service/ProjectServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.csv.communitytrackerjava.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceImplTest {
+
+    @Test
+    @DisplayName("")
+    public void saveProject() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
